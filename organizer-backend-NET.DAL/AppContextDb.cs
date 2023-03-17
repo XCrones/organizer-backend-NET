1f778b90b053588e0fc1671e9d424b87902b5be6
XCrone <ls.xcrone@gmail.com>
2023-03-18 00:21:27 +0300

    add: ForecastDB & WeatherDB

--- organizer-backend-NET.DAL/AppContextDb.cs
+++ organizer-backend-NET.DAL/AppContextDb.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using organizer_backend_NET.Domain.Entity.Calendar;
 using organizer_backend_NET.Domain.Entity.Todo;
+using organizer_backend_NET.Domain.Entity.Weather;
 
 namespace organizer_backend_NET.DAL
 {
@@ -13,6 +14,10 @@ namespace organizer_backend_NET.DAL
         public DbSet<Todo> TodoDB { get; set; }
 
         public DbSet<Calendar> CalendarDB { get; set; }
+
+        public DbSet<Forecast> ForecastDB { get; set; }
+
+        public DbSet<WeatherUser> WeatherUserDB { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder) {
 
@@ -48,6 +53,35 @@ namespace organizer_backend_NET.DAL
                 entity.Property(e => e.EventEnd).HasColumnName("EventEnd");
                 entity.Property(e => e.Description).HasColumnName("Description").HasMaxLength(100);
                 entity.Property(e => e.Background).HasColumnName("Background").HasMaxLength(20);
+
+                entity.Property(e => e.CreatedAt).HasColumnName("CreatedAt");
+                entity.Property(e => e.UpdatedAt).HasColumnName("UpdatedAt");
+                entity.Property(e => e.DeleteAt).HasColumnName("DeleteAt");
+            });
+
+            modelBuilder.Entity<Forecast>(entity => {
+                entity.ToTable("Forecast");
+
+                entity.Property(e => e.Id).HasColumnName("Id").ValueGeneratedOnAdd();
+
+                entity.Property(e => e.cod).HasColumnName("cod");
+                entity.Property(e => e.cnt).HasColumnName("cnt");
+                entity.Property(e => e.message).HasColumnName("message");
+                entity.Property(e => e.weather).HasColumnName("weather");
+                entity.Property(e => e.city).HasColumnName("city");
+
+                entity.Property(e => e.CreatedAt).HasColumnName("CreatedAt");
+                entity.Property(e => e.UpdatedAt).HasColumnName("UpdatedAt");
+                entity.Property(e => e.DeleteAt).HasColumnName("DeleteAt");
+            });
+
+            modelBuilder.Entity<WeatherUser>(entity => {
+                entity.ToTable("Weather");
+
+                entity.Property(e => e.Id).HasColumnName("Id").ValueGeneratedOnAdd();
+
+                entity.Property(e => e.Uid).HasColumnName("Uid");
+                entity.Property(e => e.cities).HasColumnName("cities");
 
                 entity.Property(e => e.CreatedAt).HasColumnName("CreatedAt");
                 entity.Property(e => e.UpdatedAt).HasColumnName("UpdatedAt");
