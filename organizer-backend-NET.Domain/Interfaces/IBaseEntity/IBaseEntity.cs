a030961f80c2a7f253cd091868fb7a13088f8815
XCrone <ls.xcrone@gmail.com>
2023-03-17 17:43:18 +0300

    join: interface

---
+++ organizer-backend-NET.Domain/Interfaces/IBaseEntity/IBaseEntity.cs
@@ -0,0 +1,10 @@
+﻿
+using organizer_backend_NET.DAL.Interfaces.ITiming;
+
+namespace organizer_backend_NET.Domain.Interfaces.IBaseEntity
+{
+    public interface IBaseEntity: ITiming
+    {
+        public int Id { get; set; }
+    }
+}
