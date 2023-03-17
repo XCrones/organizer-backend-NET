8c52fb45c12153d6c3a1fc60d728990a791b0b35
XCrone <ls.xcrone@gmail.com>
2023-03-17 23:00:23 +0300

    add: calendar service & repository

--- organizer-backend-NET/Services.cs
+++ organizer-backend-NET/Services.cs
@@ -2,8 +2,12 @@
 using organizer_backend_NET.DAL;
 using organizer_backend_NET.DAL.Interfaces.ITodo;
 using organizer_backend_NET.DAL.Repository.Todo;
-using organizer_backend_NET.Service.Implementations.TodoService;
+using organizer_backend_NET.Service.Services.TodoService;
 using organizer_backend_NET.Service.Interfaces.ITodo;
+using organizer_backend_NET.DAL.Interfaces.ICalendar;
+using organizer_backend_NET.DAL.Repository.Calendar;
+using organizer_backend_NET.Service.Interfaces.ICalendarService;
+using organizer_backend_NET.Service.Services.CalendarService;
 
 namespace organizer_backend_NET
 {
@@ -12,11 +16,13 @@ namespace organizer_backend_NET
         public static void InitRepositories(this WebApplicationBuilder builder)
         {
             builder.Services.AddScoped<ITodoRespository, TodoRepository>();
+            builder.Services.AddScoped<ICalendarRepository, CalendarRepository>();
         }
 
         public static void InitServices(this WebApplicationBuilder builder)
         {
             builder.Services.AddScoped<ITodoService, TodoService>();
+            builder.Services.AddScoped<ICalendarService, CalendarService>();
         }
 
         public static void InitDataBase(this WebApplicationBuilder builder)
