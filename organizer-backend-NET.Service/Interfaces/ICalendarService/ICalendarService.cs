3bb47ca01919a5fd7fb89fd449002ecf63048e45
XCrone <ls.xcrone@gmail.com>
2023-03-17 17:45:23 +0300

    join: interface

---
+++ organizer-backend-NET.Service/Interfaces/ICalendarService/ICalendarService.cs
@@ -0,0 +1,11 @@
+﻿
+using organizer_backend_NET.Domain.Entity.Calendar;
+using organizer_backend_NET.Domain.ViewModel.Calendar;
+using organizer_backend_NET.Service.Interfaces.IBaseService;
+
+namespace organizer_backend_NET.Service.Interfaces.ICalendarService
+{
+    public interface ICalendarService: IBaseService<Calendar, CalendarViewModel>
+    {
+    }
+}
