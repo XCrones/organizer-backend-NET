f9c072207e47e7a159b515f0e8827210e7afcf37
XCrone <ls.xcrone@gmail.com>
2023-03-17 22:33:02 +0300

    add: repository

---
+++ organizer-backend-NET.DAL/Repository/Calendar/CalendarRepository.cs
@@ -0,0 +1,38 @@
+﻿
+using organizer_backend_NET.DAL.Interfaces.ICalendar;
+
+namespace organizer_backend_NET.DAL.Repository.Calendar
+{
+    public class CalendarRepository : ICalendarRepository
+    {
+        private readonly AppContextDb _db;
+
+        public CalendarRepository(AppContextDb db)
+        {
+            _db = db;
+        }
+
+        public async Task<bool> Create(Domain.Entity.Calendar.Calendar entity)
+        {
+            await _db.CalendarDB.AddAsync(entity);
+            await _db.SaveChangesAsync();
+            return true;
+        }
+
+        public async Task<bool> Delete(Domain.Entity.Calendar.Calendar entity)
+        {
+            _db.CalendarDB.Remove(entity);
+            await _db.SaveChangesAsync();
+            return true;
+        }
+
+        public IQueryable<Domain.Entity.Calendar.Calendar> Read() => _db.CalendarDB;
+
+        public async Task<Domain.Entity.Calendar.Calendar> Update(Domain.Entity.Calendar.Calendar entity)
+        {
+            _db.CalendarDB.Update(entity);
+            await _db.SaveChangesAsync();
+            return entity;
+        }
+    }
+}
