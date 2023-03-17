df18db0eade9f411457eb56d417b1540819a164b
XCrone <ls.xcrone@gmail.com>
2023-03-18 00:16:16 +0300

    join: entity

---
+++ organizer-backend-NET.Domain/Entity/Weather/WeatherUser.cs
@@ -0,0 +1,30 @@
+﻿using organizer_backend_NET.Domain.Interfaces.IBaseEntity;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace organizer_backend_NET.Domain.Entity.Weather
+{
+    public class WeatherUser : IBaseEntity
+    {
+        public int Id { get; set; }
+
+        public int Uid { get; set; }
+
+        [Column(TypeName = "jsonb")]
+        public List<Cities> cities { get; set; }
+
+        public DateTime CreatedAt { get; set; }
+
+        public DateTime UpdatedAt { get; set; }
+
+        public DateTime? DeleteAt { get; set; }
+    }
+
+    public class Cities
+    {
+        public int id { get; set; }
+
+        public string name { get; set; }
+
+        public string country { get; set; }
+    }
+}
