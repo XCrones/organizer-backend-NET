038704b9ba8e8878126bbf12dad7fe6f0807b474
XCrone <ls.xcrone@gmail.com>
2023-03-17 10:27:13 +0300

    add: get by id, save new

--- organizer-backend-NET/Controllers/TodoController.cs
+++ organizer-backend-NET/Controllers/TodoController.cs
@@ -1,10 +1,13 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using organizer_backend_NET.Domain.Entity.Todo;
+using organizer_backend_NET.Domain.Interfaces.IResponse;
+using organizer_backend_NET.Domain.ViewModel.Todo;
 using organizer_backend_NET.Service.Interfaces.ITodo;
 
 namespace organizer_backend_NET.Controllers
 {
     [ApiController]
-    [Route("[controller]")]
+    [Route("api/[controller]")]
     public class TodoController : Controller
     {
         private readonly ITodoService _todoService;
@@ -14,13 +17,24 @@ namespace organizer_backend_NET.Controllers
             _todoService = todoService;
         }
 
-        [HttpGet(Name = "todo")]
-        public async Task<string> Get()
+        [HttpGet]
+        public async Task<IBaseResponse<IEnumerable<Todo>>> Get()
         {
+            return await _todoService.GetTodos();
+        }
 
-            var todos = await _todoService.GetTodos();
 
-            return "test OK";
+        [HttpGet("{id}")]
+        public async Task<IBaseResponse<Todo>> GetOne(int id)
+        {
+            return await _todoService.GetTodoById(id);
+        }
+
+        [HttpPost]
+        public async Task<IBaseResponse<bool>> Save(TodoViewModel model)
+        {
+            return await _todoService.CreateTodo(model);
+
         }
     }
 }
