823a783546d6195a6f6afd76f0737d5f52fe5441
XCrone <ls.xcrone@gmail.com>
2023-03-17 17:42:35 +0300

    replace: to IBaseEntity

--- organizer-backend-NET.Domain/Entity/Todo/Todo.cs
+++ organizer-backend-NET.Domain/Entity/Todo/Todo.cs
@@ -1,22 +1,32 @@
-﻿using organizer_backend_NET.DAL.Interfaces.ITiming;
-using organizer_backend_NET.Domain.Enum;
+﻿using organizer_backend_NET.Domain.Enum;
+using organizer_backend_NET.Domain.Interfaces.IBaseEntity;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace organizer_backend_NET.Domain.Entity.Todo
 {
     [Table("Todo")]
-    public class Todo : ITiming
+    public class Todo : IBaseEntity
     {
         public int Id { get; set; }
+
         public int Uid { get; set; }
+
         public string Name { get; set; }
+
         public string Category { get; set; }
+
         public EPriority Priority { get; set; }
+
         public DateTime DeadLine { get; set; }
+
         public bool Status { get; set; }
+
         public string Background { get; set; }
+
         public DateTime CreatedAt { get; set; }
+
         public DateTime UpdatedAt { get; set; }
+
         public DateTime? DeleteAt { get; set; }
     }
 }
