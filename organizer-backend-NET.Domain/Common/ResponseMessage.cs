631ae7ebb859c54befc7f1059d1a8b3b115d03de
XCrone <ls.xcrone@gmail.com>
2023-03-17 13:22:50 +0300

    add: new messages

--- organizer-backend-NET.Domain/Common/ResponseMessage.cs
+++ organizer-backend-NET.Domain/Common/ResponseMessage.cs
@@ -3,5 +3,13 @@
     public static class ResponseMessage
     {
         public static string NOT_FOUND = "Not found";
+
+        public static string CREATE_SUCCES = "Create succes";
+
+        public static string RESTORE_SUCCES = "Restore succes";
+
+        public static string DELETE_SUCCES = "Delete succes";
+
+        public static string UPDATE_SUCCES = "Update succes";
     }
 }
