2b7c0bea310c3c7262af9b30f6202bc2dd8a2d00
XCrone <ls.xcrone@gmail.com>
2023-03-17 17:42:20 +0300

    ref: fields & implement

--- organizer-backend-NET.Domain/Entity/Calendar/Calendar.cs
+++ organizer-backend-NET.Domain/Entity/Calendar/Calendar.cs
@@ -1,23 +1,27 @@
-﻿namespace organizer_backend_NET.Domain.Entity.Calendar
+﻿using organizer_backend_NET.Domain.Interfaces.IBaseEntity;
+
+namespace organizer_backend_NET.Domain.Entity.Calendar
 {
-    public class Calendar
+    public class Calendar : IBaseEntity
     {
         public int Id { get; set; }
 
-        public int? Uid { get; set; }
+        public int Uid { get; set; }
 
-        public DateTime? EventStart { get; set; }
+        public string Name { get; set; }
 
-        public DateTime? EventEnd { get; set; }
+        public DateTime EventStart { get; set; }
 
-        public string? Title { get; set; }
+        public DateTime EventEnd { get; set; }
 
-        public string? Description { get; set; }
+        public string Description { get; set; }
 
-        public string? Background { get; set; }
+        public string Background { get; set; }
 
         public DateTime CreatedAt { get; set; }
 
         public DateTime UpdatedAt { get; set; }
+
+        public DateTime? DeleteAt { get; set; }
     }
 }
