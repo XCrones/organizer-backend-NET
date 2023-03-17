bebaf561873178dbbb159b15f271a10040982966
XCrone <ls.xcrone@gmail.com>
2023-03-17 17:41:43 +0300

    join: interface

---
+++ organizer-backend-NET.DAL/Interfaces/ICalendar/ICalendarRepository.cs
@@ -0,0 +1,9 @@
+﻿using organizer_backend_NET.DAL.Interfaces.IBase;
+using organizer_backend_NET.Domain.Entity.Calendar;
+
+namespace organizer_backend_NET.DAL.Interfaces.ICalendar
+{
+    public interface ICalendarRepository: IBaseRepository<Calendar>
+    {
+    }
+}
