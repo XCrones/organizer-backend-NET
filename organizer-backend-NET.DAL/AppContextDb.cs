cce15d8517a67a488020486efba1d3fffbd15e41
XCrone <ls.xcrone@gmail.com>
2023-03-17 13:28:07 +0300

    add: config todo table

--- organizer-backend-NET.DAL/AppContextDb.cs
+++ organizer-backend-NET.DAL/AppContextDb.cs
@@ -11,5 +11,28 @@ namespace organizer_backend_NET.DAL
         }
 
         public DbSet<Todo> TodoDB { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder) {
+
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Todo>(entity => {
+                entity.ToTable("Todo");
+
+                entity.Property(e => e.Id).HasColumnName("Id").ValueGeneratedOnAdd();
+                entity.Property(e => e.Name).HasColumnName("Name").HasMaxLength(50);
+                entity.Property(e => e.Category).HasColumnName("Category").HasMaxLength(50);
+                entity.Property(e => e.Priority).HasColumnName("Priority");
+                entity.Property(e => e.DeadLine).HasColumnName("DeadLine");
+                entity.Property(e => e.Status).HasColumnName("Status");
+                entity.Property(e => e.Background).HasColumnName("Background").HasMaxLength(20);
+
+                entity.Property(e => e.CreatedAt).HasColumnName("CreatedAt");
+                entity.Property(e => e.UpdatedAt).HasColumnName("UpdatedAt");
+                entity.Property(e => e.DeleteAt).HasColumnName("DeleteAt");
+            });
+
+        }
+
     }
 }
