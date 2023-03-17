ff4fa993ddba14d21e7e4cbabc4212d550b0d002
XCrone <ls.xcrone@gmail.com>
2023-03-17 13:26:13 +0300

    ref: implements of IBaseService

--- organizer-backend-NET.Service/Interfaces/ITodo/ITodoService.cs
+++ organizer-backend-NET.Service/Interfaces/ITodo/ITodoService.cs
@@ -1,21 +1,10 @@
 ﻿using organizer_backend_NET.Domain.Entity.Todo;
-using organizer_backend_NET.Domain.Interfaces.IResponse;
 using organizer_backend_NET.Domain.ViewModel.Todo;
+using organizer_backend_NET.Service.Interfaces.IBaseService;
 
 namespace organizer_backend_NET.Service.Interfaces.ITodo
 {
-    public interface ITodoService
+    public interface ITodoService : IBaseService<Todo, TodoViewModel>
     {
-        Task<IBaseResponse<IEnumerable<Todo>>> GetTodos();
-
-        Task<IBaseResponse<Todo>> GetTodoById(int id);
-
-        Task<IBaseResponse<Todo>> GetTodoByName(string name);
-
-        Task<IBaseResponse<bool>> DeleteTodo(int id);
-
-        Task<IBaseResponse<bool>> CreateTodo(TodoViewModel todoViewModel);
-
-        Task<IBaseResponse<Todo>> EditTodo(int id, TodoViewModel todoViewModel);
     }
 }
