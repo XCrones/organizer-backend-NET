11f2f7abe39dfd433ee1515db51680fa996e21a2
XCrone <ls.xcrone@gmail.com>
2023-03-17 13:23:38 +0300

    rename: file

---
+++ organizer-backend-NET.Domain/Enum/EPriority.cs
@@ -0,0 +1,17 @@
+﻿
+using System.ComponentModel;
+
+namespace organizer_backend_NET.Domain.Enum
+{
+    public enum EPriority : byte
+    {
+        [Description("Hight")]
+        HIGHT = 0,
+
+        [Description("Medium")]
+        MEDIUM = 1,
+
+        [Description("Low")]
+        LOW = 2,
+    }
+}
