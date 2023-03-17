a5cdb3e39eab0e336bf922f4278e1024fc13af4c
XCrone <ls.xcrone@gmail.com>
2023-03-17 17:42:55 +0300

    join: interface

--- organizer-backend-NET.Domain/Entity/User/User.cs
+++ organizer-backend-NET.Domain/Entity/User/User.cs
@@ -1,19 +1,23 @@
-﻿namespace organizer_backend_NET.Domain.Entity.User
+﻿using organizer_backend_NET.DAL.Interfaces.ITiming;
+
+namespace organizer_backend_NET.Domain.Entity.User
 {
-    public class User
+    public class User : ITiming
     {
-        public int Id { get; set; }
+        public int UId { get; set; }
 
-        public string? Email { get; set; }
+        public string Name { get; set; }
 
-        public string? Name { get; set; }
+        public string Email { get; set; }
 
-        public string? Password { get; set; }
+        public string Password { get; set; }
 
-        public string? UrlAvatar { get; set; }
+        public string UrlAvatar { get; set; }
 
         public DateTime CreatedAt { get; set; }
 
         public DateTime UpdatedAt { get; set; }
+
+        public DateTime? DeleteAt { get; set; }
     }
 }
