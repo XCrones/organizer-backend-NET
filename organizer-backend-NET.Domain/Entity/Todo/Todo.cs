3063b0502aaace02bf6e7b91ea2dd385b23f974d
XCrone <ls.xcrone@gmail.com>
2023-03-17 13:23:05 +0300

    rename: enum

--- organizer-backend-NET.Domain/Entity/Todo/Todo.cs
+++ organizer-backend-NET.Domain/Entity/Todo/Todo.cs
@@ -11,11 +11,12 @@ namespace organizer_backend_NET.Domain.Entity.Todo
         public int Uid { get; set; }
         public string Name { get; set; }
         public string Category { get; set; }
-        public TPriority Priority { get; set; }
+        public EPriority Priority { get; set; }
         public DateTime DeadLine { get; set; }
         public bool Status { get; set; }
         public string Background { get; set; }
         public DateTime CreatedAt { get; set; }
         public DateTime UpdatedAt { get; set; }
+        public DateTime? DeleteAt { get; set; }
     }
 }
