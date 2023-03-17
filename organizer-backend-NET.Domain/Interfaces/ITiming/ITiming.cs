8a8ea7c601856a33aa3397a24634da07be4dc671
XCrone <ls.xcrone@gmail.com>
2023-03-17 10:23:37 +0300

    join: timing DB

---
+++ organizer-backend-NET.Domain/Interfaces/ITiming/ITiming.cs
@@ -0,0 +1,8 @@
+﻿namespace organizer_backend_NET.DAL.Interfaces.ITiming
+{
+    public interface ITiming
+    {
+        DateTime CreatedAt { get; set; }
+        DateTime UpdatedAt { get; set; }
+    }
+}
