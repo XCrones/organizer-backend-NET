0278854a7eb7c91f1478fa33c2f70162943429db
XCrone <ls.xcrone@gmail.com>
2023-03-18 12:27:43 +0300

    rename: entity

---
+++ organizer-backend-NET.Domain/Entity/UserWeather.cs
@@ -0,0 +1,39 @@
+﻿using organizer_backend_NET.Domain.Interfaces.IBaseEntity;
+using organizer_backend_NET.Domain.Interfaces.IUId;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace organizer_backend_NET.Domain.Entity
+{
+    [Table("UserWeathers")]
+    public class UserWeather : IBaseEntity, IUId
+    {
+        [Column("Id"), DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [Column("UId"), ForeignKey("User")]
+        public int UId { get; set; }
+
+        [Column("Cities", TypeName = "jsonb")]
+        public List<CityWeather> Cities { get; set; }
+
+        [Column("CreatedAt")]
+        public DateTime CreatedAt { get; set; }
+
+        [Column("UpdatedAt")]
+        public DateTime UpdatedAt { get; set; }
+
+        [Column("DeleteAt")]
+        public DateTime? DeleteAt { get; set; }
+
+        public virtual User User { get; set; }
+    }
+
+    public class CityWeather
+    {
+        public int id { get; set; }
+
+        public string name { get; set; }
+
+        public string country { get; set; }
+    }
+}
