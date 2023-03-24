80c8ea8a1ed88a812a9e2684f44ac3ca8c9e873e
Serjey <ls.xcrone@gmail.com>
2023-03-24 15:38:12 +0300

    join: InitJWTAuth

--- organizer-backend-NET/Services.cs
+++ organizer-backend-NET/Services.cs
@@ -4,6 +4,9 @@ using organizer_backend_NET.Implements.Services;
 using organizer_backend_NET.Implements.Interfaces;
 using organizer_backend_NET.DAL.Interfaces;
 using organizer_backend_NET.DAL.Repository;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 
 namespace organizer_backend_NET
 {
@@ -26,6 +29,34 @@ namespace organizer_backend_NET
         public static void InitDataBase(this WebApplicationBuilder builder)
         {
             builder.Services.AddDbContext<AppContextDb>(o => o.UseNpgsql(builder.Configuration.GetConnectionString("AppContextDb"))); //Integrated Security=true;Pooling=true;
+        }
+
+        public static void InitJWTAuth(this WebApplicationBuilder builder)
+        {
+            builder.Services.Configure<JWTSettings>(builder.Configuration.GetSection("JWTSettings"));
+
+            var secretKey = builder.Configuration.GetSection("JWTSettings:SecretKey").Value;
+            var issuer = builder.Configuration.GetSection("JWTSettings:Issuer").Value;
+            var audience = builder.Configuration.GetSection("JWTSettings:Audience").Value;
+            var signinkey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(secretKey));
+
+            builder.Services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+            }).AddJwtBearer(options => {
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuer = true,
+                    ValidIssuer = issuer,
+                    ValidateAudience = true,
+                    ValidAudience = audience,
+                    ValidateLifetime = true,
+                    IssuerSigningKey = signinkey,
+                    ValidateIssuerSigningKey = true,
+                };
+            });
         }
     }
 }
