8ffd5de944be87a8833a47a5872126bf77f5f43f
Serjey <ls.xcrone@gmail.com>
2023-03-24 09:21:16 +0300

    add: login controller

---
+++ organizer-backend-NET/Controllers/LoginController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using organizer_backend_NET.Domain.Entity;
+using organizer_backend_NET.Domain.Interfaces;
+using organizer_backend_NET.Domain.ViewModel;
+using organizer_backend_NET.Implements.Interfaces;
+
+namespace organizer_backend_NET.Controllers
+{
+    [ApiController, Route("api/[controller]")]
+    public class LoginController : Controller
+    {
+        private readonly IUserService _userService;
+
+        public LoginController(IUserService userService)
+        {
+            _userService = userService;
+        }
+
+        [HttpPost("signup")]
+        public async Task<IBaseResponse<User>> SignUp(SignupViewModel model)
+        {
+            return await _userService.SignUp(model);
+        }
+    }
+}
