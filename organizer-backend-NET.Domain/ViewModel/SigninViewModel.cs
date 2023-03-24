b5c7a04452812963836baee8fc24a402591a54e6
Serjey <ls.xcrone@gmail.com>
2023-03-24 09:18:57 +0300

    join: login view models

---
+++ organizer-backend-NET.Domain/ViewModel/SigninViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace organizer_backend_NET.Domain.ViewModel
+{
+    public class SigninViewModel
+    {
+        [Required, DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+
+        [Required, StringLength(50, MinimumLength = 6)]
+        public string Password { get; set; }
+    }
+}
---
+++ organizer-backend-NET.Domain/ViewModel/SignupViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace organizer_backend_NET.Domain.ViewModel
+{
+    public class SignupViewModel
+    {
+        [Required, StringLength(50, MinimumLength = 3)]
+        public string Name { get; set; }
+
+        [Required, DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+
+        [Required, StringLength(50, MinimumLength = 6)]
+        public string Password { get; set; }
+
+        [Required, Compare("Password")]
+        public string ComparePassword { get; set; }
+
+        [Required, DataType(DataType.Url)]
+        public string? UrlAvatar { get; set; }
+    }
+}
