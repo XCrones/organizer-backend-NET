5340c43c2563ff553259498d2e058b981f2a1bf6
Serjey <ls.xcrone@gmail.com>
2023-03-24 09:21:35 +0300

    rename: folder services to implements

--- organizer-backend-NET/Services.cs
+++ organizer-backend-NET/Services.cs
@@ -1,7 +1,7 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using organizer_backend_NET.DAL;
-using organizer_backend_NET.Service.Services;
-using organizer_backend_NET.Service.Interfaces;
+using organizer_backend_NET.Implements.Services;
+using organizer_backend_NET.Implements.Interfaces;
 using organizer_backend_NET.DAL.Interfaces;
 using organizer_backend_NET.DAL.Repository;
 
@@ -13,12 +13,14 @@ namespace organizer_backend_NET
         {
             builder.Services.AddScoped<ITodoRespository, TodoRepository>();
             builder.Services.AddScoped<ICalendarRepository, CalendarRepository>();
+            builder.Services.AddScoped<IUserRepository, UserRepository>();
         }
 
         public static void InitServices(this WebApplicationBuilder builder)
         {
             builder.Services.AddScoped<ITodoService, TodoService>();
             builder.Services.AddScoped<ICalendarService, CalendarService>();
+            builder.Services.AddScoped<IUserService, UserService>();
         }
 
         public static void InitDataBase(this WebApplicationBuilder builder)
