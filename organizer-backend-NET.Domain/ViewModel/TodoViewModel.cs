1c626a9fb0991e55827abfd4ff4e96d4bb8e88e7
Serjey <ls.xcrone@gmail.com>
2023-03-24 11:29:25 +0300

    ref: annotations

--- organizer-backend-NET.Domain/ViewModel/TodoViewModel.cs
+++ organizer-backend-NET.Domain/ViewModel/TodoViewModel.cs
@@ -4,18 +4,20 @@ namespace organizer_backend_NET.Domain.ViewModel
 {
     public class TodoViewModel
     {
-        public int Id { get; set; }
 
-        [Required, StringLength(50, MinimumLength = 3)]
+        [Required]
+        [StringLength(50, MinimumLength = 3)]
         public string Name { get; set; }
 
-        [Required, StringLength(50, MinimumLength = 3)]
+        [Required]
+        [StringLength(50, MinimumLength = 3)]
         public string Category { get; set; }
 
-        [Required, StringLength(1, MinimumLength = 1)]
-        public string Priority { get; set; } //!
+        [Required]
+        public int Priority { get; set; } //!
 
-        [Required, DataType(DataType.DateTime)]
+        [Required]
+        [DataType(DataType.DateTime)]
         public DateTime DeadLine { get; set; }
 
         [Required]
