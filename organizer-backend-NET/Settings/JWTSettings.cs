0dda3fcd1ff772616f34da09894b46704e53d37c
Serjey <ls.xcrone@gmail.com>
2023-03-24 18:18:09 +0300

    ref: move to settings folder

--- organizer-backend-NET/Settings/JWTSettings.cs
+++ organizer-backend-NET/Settings/JWTSettings.cs
@@ -1,12 +1,11 @@
-﻿namespace organizer_backend_NET
+﻿namespace organizer_backend_NET.Settings
 {
     public class JWTSettings
     {
         public string SecretKey { get; set; }
 
-        public string Issuer { get; set; } 
+        public string Issuer { get; set; }
 
         public string Audience { get; set; }
-
     }
 }
