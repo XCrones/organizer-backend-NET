e386b1f68c012a8f3ac5ed75f4418cbe1286601e
Serjey <ls.xcrone@gmail.com>
2023-03-24 09:21:02 +0300

    join: user respository

---
+++ organizer-backend-NET.DAL/Repository/UserRepository.cs
@@ -0,0 +1,38 @@
+﻿using organizer_backend_NET.DAL.Interfaces;
+using organizer_backend_NET.Domain.Entity;
+
+namespace organizer_backend_NET.DAL.Repository
+{
+    public class UserRepository : IUserRepository
+    {
+        private readonly AppContextDb _db;
+
+        public UserRepository(AppContextDb db)
+        {
+            _db = db;
+        }
+
+        public async Task<bool> Create(User entity)
+        {
+            await _db.UserDB.AddAsync(entity);
+            await _db.SaveChangesAsync();
+            return true;
+        }
+
+        public async Task<bool> Remove(User entity)
+        {
+            _db.UserDB.Remove(entity);
+            await _db.SaveChangesAsync();
+            return true;
+        }
+
+        public IQueryable<User> Read() => _db.UserDB;
+
+        public async Task<User> Update(User entity)
+        {
+            _db.UserDB.Update(entity);
+            await _db.SaveChangesAsync();
+            return entity;
+        }
+    }
+}
