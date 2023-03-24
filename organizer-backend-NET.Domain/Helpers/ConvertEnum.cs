e965dd1b68f21d7159ed4af391b3eeaeea385013
Serjey <ls.xcrone@gmail.com>
2023-03-24 11:28:30 +0300

    join: enum conver

---
+++ organizer-backend-NET.Domain/Helpers/ConvertEnum.cs
@@ -0,0 +1,26 @@
+﻿
+using organizer_backend_NET.Domain.Enums;
+
+namespace organizer_backend_NET.Domain.Helpers
+{
+    public static class ConvertEnum
+    {
+        public static EPriority PriorityIntToEnum(int value)
+        {
+            try
+            {
+                if (Enum.IsDefined(typeof(EPriority), value))
+                {
+                    return (EPriority)value;
+                }
+
+                return EPriority.low;
+            }
+            catch (Exception ex)
+            {
+                //! loggin error
+                return EPriority.low;
+            }
+        }
+    }
+}
---
+++ organizer-backend-NET.Domain/Helpers/HashPasswordHelper.cs
@@ -0,0 +1,19 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace organizer_backend_NET.Domain.Helpers
+{
+    public static class HashPasswordHelper
+    {
+        public static string HashPassword(string password)
+        {
+            using (var sha256 = SHA256.Create())
+            {
+                var hashBytes = sha256.ComputeHash(Encoding.UTF8.GetBytes(password));
+                var hash = BitConverter.ToString(hashBytes).Replace("-", "").ToLower();
+
+                return hash;
+            }
+        }
+    }
+}
