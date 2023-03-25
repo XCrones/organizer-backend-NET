4cf26a88dd28e86c86d24a346dcd71a55006a9a9
Serjey <ls.xcrone@gmail.com>
2023-03-25 12:06:44 +0300

    rm: type data response

--- organizer-backend-NET/Interfaces/IControllers/ICalendar-Controller.cs
+++ organizer-backend-NET/Interfaces/IControllers/ICalendar-Controller.cs
@@ -1,9 +1,8 @@
-﻿using organizer_backend_NET.Domain.Entity;
-using organizer_backend_NET.Domain.ViewModel;
+﻿using organizer_backend_NET.Domain.ViewModel;
 
 namespace organizer_backend_NET.Interfaces.IControllers
 {
-    public interface ICalendar_Controller : IBase_Contorller<Calendar, CalendarViewModel>
+    public interface ICalendar_Controller : IBase_Contorller<CalendarViewModel>
     {
     }
 }
--- organizer-backend-NET/Interfaces/IControllers/ITodo-Controller.cs
+++ organizer-backend-NET/Interfaces/IControllers/ITodo-Controller.cs
@@ -1,9 +1,8 @@
-﻿using organizer_backend_NET.Domain.Entity;
-using organizer_backend_NET.Domain.ViewModel;
+﻿using organizer_backend_NET.Domain.ViewModel;
 
 namespace organizer_backend_NET.Interfaces.IControllers
 {
-    public interface ITodo_Controller: IBase_Contorller<Todo, TodoViewModel>
+    public interface ITodo_Controller: IBase_Contorller<TodoViewModel>
     {
     }
 }
