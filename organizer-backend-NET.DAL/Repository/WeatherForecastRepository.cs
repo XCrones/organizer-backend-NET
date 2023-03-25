e998dcf7b33031146b1db96b3ae4b2f8edcbdf75
Serjey <ls.xcrone@gmail.com>
2023-03-25 13:52:16 +0300

    join weather forecast repository & init

---
+++ organizer-backend-NET.DAL/Repository/WeatherForecastRepository.cs
@@ -0,0 +1,38 @@
+﻿using organizer_backend_NET.DAL.Interfaces;
+using organizer_backend_NET.Domain.Entity;
+
+namespace organizer_backend_NET.DAL.Repository
+{
+    public class WeatherForecastRepository : IWeatherForecastRepository
+    {
+        private readonly AppContextDb _db;
+
+        public WeatherForecastRepository(AppContextDb db)
+        {
+            _db = db;
+        }
+
+        public async Task<bool> Create(WeatherForecast entity)
+        {
+            await _db.ForecastDB.AddAsync(entity);
+            await _db.SaveChangesAsync();
+            return true;
+        }
+
+        public async Task<bool> Delete(WeatherForecast entity)
+        {
+            _db.ForecastDB.Remove(entity);
+            await _db.SaveChangesAsync();
+            return true;
+        }
+
+        public IQueryable<WeatherForecast> Read() => _db.ForecastDB;
+
+        public async Task<WeatherForecast> Update(WeatherForecast entity)
+        {
+            _db.ForecastDB.Update(entity);
+            await _db.SaveChangesAsync();
+            return entity;
+        }
+    }
+}
--- organizer-backend-NET/Inits/RepositoriesInit.cs
+++ organizer-backend-NET/Inits/RepositoriesInit.cs
@@ -10,6 +10,7 @@ namespace organizer_backend_NET.Services
             builder.Services.AddScoped<ITodoRespository, TodoRepository>();
             builder.Services.AddScoped<ICalendarRepository, CalendarRepository>();
             builder.Services.AddScoped<IUserRepository, UserRepository>();
+            builder.Services.AddScoped<IWeatherForecastRepository, WeatherForecastRepository>();
         }
     }
 }
