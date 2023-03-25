f1668dc0187cbde33de42caf5aaed872550d443f
Serjey <ls.xcrone@gmail.com>
2023-03-25 13:27:59 +0300

    join: Action Responses

---
+++ organizer-backend-NET/Response/LoginResponse.cs
@@ -0,0 +1,16 @@
+﻿using organizer_backend_NET.Domain.Entity;
+using System.Net;
+
+namespace organizer_backend_NET.Response
+{
+    public class LoginResponse
+    {
+        public HttpStatusCode Code { get; set; }
+
+        public string? Message { get; set; }
+
+        public User Data { get; set; }
+
+        public string Token { get; set; }
+    }
+}
