99caf450d0130682bc4aaef18b0609d88ec4e460
Serjey <ls.xcrone@gmail.com>
2023-03-25 12:04:37 +0300

    replace: enum message to static class

---
+++ organizer-backend-NET.Domain/Messages/AppMessages.cs
@@ -0,0 +1,23 @@
+﻿namespace organizer_backend_NET.Domain.Messages
+{
+    public static class AppMessages
+    {
+        public static string NotFound = "Not found";
+
+        public static string CreateSucces = "Create succes";
+
+        public static string RestoreSucces = "Restore succes";
+
+        public static string RemoveSucces = "Remove succes";
+
+        public static string UpdateSucces = "Update succes";
+
+        public static string EmailIsBusy = "Email is busy";
+
+        public static string NewItemNotFound = "New item not found";
+
+        public static string IncorrectEmilOrPassword = "Incorrect email or password";
+
+        public static string UserNotFound = "User not found";
+    }
+}
