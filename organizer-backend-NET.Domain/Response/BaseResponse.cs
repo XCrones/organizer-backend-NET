0443087c884b34b1628e3f4b01561120f428943f
Serjey <ls.xcrone@gmail.com>
2023-03-25 13:26:53 +0300

    replace: Estatus to System Https status

--- organizer-backend-NET.Domain/Response/BaseResponse.cs
+++ organizer-backend-NET.Domain/Response/BaseResponse.cs
@@ -1,12 +1,12 @@
-﻿using organizer_backend_NET.Domain.Enums;
-using organizer_backend_NET.Domain.Interfaces;
+﻿using organizer_backend_NET.Domain.Interfaces;
+using System.Net;
 
 namespace organizer_backend_NET.Domain.Response
 {
     public class BaseResponse<T> : IBaseResponse<T>
     {
         public string? Description { get; set; }
-        public EStatusCode StatusCode { get; set; }
+        public HttpStatusCode StatusCode { get; set; }
         public T? Data { get; set; }
     }
 }
