368ba2f0326217a9ca7bcc71ef71019008d3c9e9
Serjey <ls.xcrone@gmail.com>
2023-03-25 13:45:44 +0300

    join: Http client service & init

---
+++ organizer-backend-NET.Service/Implements/HttpClientService.cs
@@ -0,0 +1,12 @@
+﻿using organizer_backend_NET.Service.Interfaces;
+
+namespace organizer_backend_NET.Service.Implements
+{
+    public class HttpClientService : IHttpClientService
+    {
+        public Task<TD?> Get<TD>(string url)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
