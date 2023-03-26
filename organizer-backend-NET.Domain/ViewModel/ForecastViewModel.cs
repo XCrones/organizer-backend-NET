904e503fed3448eb3fb71d6369cb845d23ac0e60
Serjey <ls.xcrone@gmail.com>
2023-03-26 12:25:11 +0300

    ref: types int to float

---
+++ organizer-backend-NET.Domain/ViewModel/ForecastViewModel.cs
@@ -0,0 +1,18 @@
+﻿
+using organizer_backend_NET.Domain.Entity;
+
+namespace organizer_backend_NET.Domain.ViewModel
+{
+    public class ForecastViewModel
+    {
+        public string cod { get; set; }
+
+        public int message { get; set; }
+
+        public int cnt { get; set; }
+
+        public List<WeatherItem> list { get; set; }
+
+        public CityForecast city { get; set; }
+    }
+}
