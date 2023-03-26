3f13c0cc521f8c3cc352b956508d3fed056b68e5
Serjey <ls.xcrone@gmail.com>
2023-03-26 12:27:13 +0300

    join: service

---
+++ organizer-backend-NET.Service/Implements/OpenWeatherService.cs
@@ -0,0 +1,91 @@
+﻿using organizer_backend_NET.Domain.Interfaces;
+using organizer_backend_NET.Domain.Response;
+using organizer_backend_NET.Domain.ViewModel;
+using organizer_backend_NET.Service.Interfaces;
+using System.Net;
+
+namespace organizer_backend_NET.Service.Implements
+{
+    public class OpenWeatherService : IOpenWeatherService
+    {
+        private readonly IHttpClientService _httpClientService;
+
+        private readonly string _token = "c42021649078332395ebebbbd1302a0d";
+        private readonly string _urlApi = "https://api.openweathermap.org/data/2.5";
+        // private readonly string _urlIcon = "http://openweathermap.org/img/wn";
+        // private readonly string _prefixIcon = "@2x.png";
+
+        public OpenWeatherService(IHttpClientService httpClientService)
+        {
+            _httpClientService = httpClientService;
+        }
+
+        private async Task<ForecastViewModel?> FetchForecast(string queries)
+        {
+            var result = await _httpClientService.Get<ForecastViewModel?>($"{_urlApi}/forecast/?{queries}&units=metric&appid={_token}");
+            return result;
+        }
+
+        public async Task<IBaseResponse<ForecastViewModel>> FetchByGeo(int lat, int lon)
+        {
+            try
+            {
+                var response = await FetchForecast($"lat={lat}&lon={lon}");
+
+                if (response != null && response.cod == "200")
+                {
+                    return new BaseResponse<ForecastViewModel>()
+                    {
+                        StatusCode = HttpStatusCode.OK,
+                        Data = response
+                    };
+                }
+
+                return new BaseResponse<ForecastViewModel>()
+                {
+                    StatusCode = HttpStatusCode.BadRequest,
+                    Data = response
+                };
+            }
+            catch (Exception ex)
+            {
+                return new BaseResponse<ForecastViewModel>()
+                {
+                    Description = $"[FetchByGeo] : {ex.Message}",
+                    StatusCode = HttpStatusCode.InternalServerError,
+                };
+            }
+        }
+
+        public async Task<IBaseResponse<ForecastViewModel>> FetchByName(string nameCity)
+        {
+            try
+            {
+                var response = await FetchForecast($"q={nameCity}");
+
+                if (response != null && response.cod == "200")
+                {
+                    return new BaseResponse<ForecastViewModel>()
+                    {
+                        StatusCode = HttpStatusCode.OK,
+                        Data = response
+                    };
+                }
+
+                return new BaseResponse<ForecastViewModel>()
+                {
+                    StatusCode = HttpStatusCode.BadRequest,
+                    Data = response
+                };
+            }
+            catch (Exception ex)
+            {
+                return new BaseResponse<ForecastViewModel>()
+                {
+                    Description = $"[FetchByName] : {ex.Message}",
+                    StatusCode = HttpStatusCode.InternalServerError,
+                };
+            }
+        }
+    }
+}
---
+++ organizer-backend-NET.Service/Interfaces/IOpenWeatherService.cs
@@ -0,0 +1,12 @@
+﻿using organizer_backend_NET.Domain.Interfaces;
+using organizer_backend_NET.Domain.ViewModel;
+
+namespace organizer_backend_NET.Service.Interfaces
+{
+    public interface IOpenWeatherService
+    {
+        public Task<IBaseResponse<ForecastViewModel>> FetchByName(string nameCity);
+
+        public Task<IBaseResponse<ForecastViewModel>> FetchByGeo(int lat, int lon);
+    }
+}
