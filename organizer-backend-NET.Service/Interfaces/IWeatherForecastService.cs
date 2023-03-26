26cb1c027221880de2ea954f03acf44d519511e9
Serjey <ls.xcrone@gmail.com>
2023-03-26 12:27:28 +0300

    join: service

---
+++ organizer-backend-NET.Service/Interfaces/IWeatherForecastService.cs
@@ -0,0 +1,12 @@
+﻿using organizer_backend_NET.Domain.Entity;
+using organizer_backend_NET.Domain.Interfaces;
+
+namespace organizer_backend_NET.Service.Interfaces
+{
+    public interface IWeatherForecastService
+    {
+        public Task<IBaseResponse<WeatherForecast>> SearchByName(string cityName);
+
+        public Task<IBaseResponse<WeatherForecast>> SearchByGeo(int lat, int lon);
+    }
+}
