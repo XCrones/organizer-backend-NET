4819250bc6ad4b29bf340693d4a358a5edca0e1e
Serjey <ls.xcrone@gmail.com>
2023-03-26 12:25:25 +0300

    join: weather controller

---
+++ organizer-backend-NET/Controllers/WeatherController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using organizer_backend_NET.Service.Interfaces;
+
+namespace organizer_backend_NET.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class WeatherController : ControllerBase
+    {
+        private readonly IWeatherForecastService _weatherForecastService;
+
+        public WeatherController(IWeatherForecastService weatherForecastService)
+        {
+            _weatherForecastService = weatherForecastService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetForecast(string name) {
+            var result = await _weatherForecastService.SearchByName(name);
+
+            return Ok(result);
+        }
+    }
+}
