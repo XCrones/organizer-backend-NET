b490f0f97a8d0f2faa467d252a8b5d1e790efe2c
Serjey <ls.xcrone@gmail.com>
2023-03-27 11:57:24 +0300

    join: methods

--- organizer-backend-NET.Service/Interfaces/IWeatherUserService.cs
+++ organizer-backend-NET.Service/Interfaces/IWeatherUserService.cs
@@ -1,6 +1,17 @@
-﻿namespace organizer_backend_NET.Service.Interfaces
+﻿using organizer_backend_NET.Domain.Entity;
+using organizer_backend_NET.Domain.Interfaces;
+using organizer_backend_NET.Domain.ViewModel;
+
+namespace organizer_backend_NET.Service.Interfaces
 {
     public interface IWeatherUserService
     {
+        public Task<IBaseResponse<List<CityWeather>>> GetCities(int UId);
+
+        public Task<IBaseResponse<bool>> RemoveItem(int UId, int cityId);
+
+        public Task<IBaseResponse<WeatherForecast>> SearchByCityName(int UId, string name);
+
+        public Task<IBaseResponse<WeatherForecast>> SearchByCityGeo(int UId, SearchCityByGeoViewModel model);
     }
 }
