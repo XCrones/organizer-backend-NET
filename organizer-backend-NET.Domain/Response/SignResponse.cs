aedd3aa01c2d72270b1a8d79f6616c7646419375
Serjey <ls.xcrone@gmail.com>
2023-03-28 12:15:21 +0300

    join: signIn response

---
+++ organizer-backend-NET.Domain/Response/SignResponse.cs
@@ -0,0 +1,12 @@
+﻿
+namespace organizer_backend_NET.Domain.Response
+{
+    public class SignResponse
+    {
+        public string Name { get; set; }
+
+        public string Email { get; set; }
+
+        public string UrlAvatar { get; set; }
+    }
+}
