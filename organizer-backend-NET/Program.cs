cdcf28990f2b32295ccee07c56a9caa6808d8010
Serjey <ls.xcrone@gmail.com>
2023-03-28 12:15:07 +0300

    add: nlog conf

--- organizer-backend-NET/Program.cs
+++ organizer-backend-NET/Program.cs
@@ -1,33 +1,69 @@
 using organizer_backend_NET.Inits;
 using organizer_backend_NET.Services;
 
-var builder = WebApplication.CreateBuilder(args);
+using NLog;
+using NLog.Web;
 
-// Add services to the container.
-DatabasesInit.Init(builder);
-RepositoriesInit.Init(builder);
-ServicesInit.Init(builder);
-AuthInit.Init(builder);
+// Early init of NLog to allow startup and exception logging, before host is built
+var logger = LogManager.Setup().LoadConfigurationFromAppSettings().GetCurrentClassLogger();
+logger.Debug("init main");
 
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+try
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    var builder = WebApplication.CreateBuilder(args);
+    builder.Host.UseNLog();
+
+    var MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
+
+    // Add services to the container.
+    DatabasesInit.Init(builder);
+    RepositoriesInit.Init(builder);
+    ServicesInit.Init(builder);
+    AuthInit.Init(builder);
+
+
+    builder.Services.AddCors(options =>
+    {
+        options.AddPolicy(name: MyAllowSpecificOrigins,
+            policy =>
+            {
+                policy.WithOrigins("http://localhost:5001", "http://localhost:8000")
+                .AllowAnyHeader()
+                .AllowAnyMethod();
+            });
+    });
+
+    builder.Services.AddControllers();
+    // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+    builder.Services.AddEndpointsApiExplorer();
+    builder.Services.AddSwaggerGen();
+
+    var app = builder.Build();
+
+    // Configure the HTTP request pipeline.
+    if (app.Environment.IsDevelopment())
+    {
+        app.UseSwagger();
+        app.UseSwaggerUI();
+    }
+
+    app.UseHttpsRedirection();
+
+    app.UseCors(MyAllowSpecificOrigins);
+
+    app.UseAuthentication();
+    app.UseAuthorization();
+
+    app.MapControllers();
+
+    app.Run();
+
 }
-
-app.UseHttpsRedirection();
-
-app.UseAuthentication();
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run();
+catch (Exception ex)
+{
+    logger.Error(ex, "Stopped program because of exception");
+    throw;
+} finally
+{
+    NLog.LogManager.Shutdown();
+}
